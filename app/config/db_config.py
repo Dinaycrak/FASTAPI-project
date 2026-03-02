89ea5e62ad55cc8340bbb215aade3a3f630b226d
Dinayker Lopez <dinaykerlopez5@gmail.com>
2026-03-02 21:32:52 +0000

    Actualización del proyecto

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,10 +1,9 @@
 import psycopg2
-#psql 'postgresql://neondb_owner:npg_4AEpq0CdGsNM@ep-royal-smoke-afutcih1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 def get_db_connection():
     return psycopg2.connect(
-        host="ep-royal-smoke-afutcih1-pooler.c-2.us-west-2.aws.neon.tech",
+        host="ep-crimson-scene-aiwhfi0r-pooler.c-4.us-east-1.aws.neon.tech",
         port="5432",
         user="neondb_owner",
-        password="npg_4AEpq0CdGsNM",
+        password="npg_kFc1KfRrd3Hh",
         dbname="neondb"
     )
